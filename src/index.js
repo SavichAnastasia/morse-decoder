cdfa92a550563a0e702d547e3897fb5801c0631e
Anastasia Savich <anastasia.savich.s@gmail.com>
2020-03-03 14:06:58 +0300

    feat: add morse-decoder task solution

--- src/index.js
+++ src/index.js
@@ -38,7 +38,17 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+    let arr = [];
+    for (var i = 0; i < expr.length; i += 10) {
+        arr.push(expr.slice(i, i + 10))
+    } 
+    arr = arr.map(item => {
+        item = item.replace(/11/g, '-').replace(/10/g, '.').replace(/0/g,'').replace(/\*\*\*\*\*\*\*\*\*\*/g, ' ');
+        if(item === ' ') return ' ';
+        return MORSE_TABLE[item];
+    })
+    
+    return arr.join('');
 }
 
 module.exports = {
